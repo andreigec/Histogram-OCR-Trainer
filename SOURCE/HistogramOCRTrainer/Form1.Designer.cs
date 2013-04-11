97add1e9a9793f61c6f36e6f96c8060cf2aa4781
andrei <andreigec@hotmail.com>
2013-04-11 23:04:09 +1000

    initial

---
+++ SOURCE/HistogramOCRTrainer/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace HistogramOCRTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.addletters = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.letterhistogramdisplayTB = new System.Windows.Forms.TextBox();
+            this.lettersLB = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.loadButtonB = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 515);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(515, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(515, 491);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.addletters);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.letterhistogramdisplayTB);
+            this.tabPage1.Controls.Add(this.lettersLB);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(507, 465);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Training";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // addletters
+            // 
+            this.addletters.Location = new System.Drawing.Point(9, 250);
+            this.addletters.Name = "addletters";
+            this.addletters.Size = new System.Drawing.Size(95, 23);
+            this.addletters.TabIndex = 4;
+            this.addletters.Text = "Add Letters";
+            this.addletters.UseVisualStyleBackColor = true;
+            this.addletters.Click += new System.EventHandler(this.addletters_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Histogram";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Letters in dictionary";
+            // 
+            // letterhistogramdisplayTB
+            // 
+            this.letterhistogramdisplayTB.Location = new System.Drawing.Point(127, 19);
+            this.letterhistogramdisplayTB.Multiline = true;
+            this.letterhistogramdisplayTB.Name = "letterhistogramdisplayTB";
+            this.letterhistogramdisplayTB.Size = new System.Drawing.Size(249, 225);
+            this.letterhistogramdisplayTB.TabIndex = 1;
+            // 
+            // lettersLB
+            // 
+            this.lettersLB.FormattingEnabled = true;
+            this.lettersLB.Location = new System.Drawing.Point(9, 19);
+            this.lettersLB.Name = "lettersLB";
+            this.lettersLB.Size = new System.Drawing.Size(95, 225);
+            this.lettersLB.TabIndex = 0;
+            this.lettersLB.SelectedIndexChanged += new System.EventHandler(this.lettersLB_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.loadButtonB);
+            this.tabPage2.Controls.Add(this.testTextBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(507, 465);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Testing";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.testTextBox.Location = new System.Drawing.Point(8, 97);
+            this.testTextBox.Multiline = true;
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(461, 319);
+            this.testTextBox.TabIndex = 0;
+            // 
+            // loadButtonB
+            // 
+            this.loadButtonB.Location = new System.Drawing.Point(25, 51);
+            this.loadButtonB.Name = "loadButtonB";
+            this.loadButtonB.Size = new System.Drawing.Size(183, 23);
+            this.loadButtonB.TabIndex = 1;
+            this.loadButtonB.Text = "Load Word Find to Test";
+            this.loadButtonB.UseVisualStyleBackColor = true;
+            this.loadButtonB.Click += new System.EventHandler(this.loadButtonB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 537);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox letterhistogramdisplayTB;
+        private System.Windows.Forms.ListBox lettersLB;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button addletters;
+        private System.Windows.Forms.TextBox testTextBox;
+        private System.Windows.Forms.Button loadButtonB;
+    }
+}
+
---
+++ SOURCE/HistogramOCRTrainer/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ANDREICSLIB;
+using ANDREICSLIB.ClassExtras;
+using ANDREICSLIB.NewControls;
+
+namespace HistogramOCRTrainer
+{
+    public partial class Form1 : Form
+    {
+        #region licensing
+
+        private const string AppTitle = "Histogram OCR Trainer";
+        private const double AppVersion = 0.1;
+        private const String HelpString = "";
+
+        private const String UpdatePath = "https://github.com/EvilSeven/Histogram-OCR-Trainer/zipball/master";
+        private const String VersionPath = "https://raw.github.com/EvilSeven/Histogram-OCR-Trainer/master/INFO/version.txt";
+        private const String ChangelogPath = "https://raw.github.com/EvilSeven/Histogram-OCR-Trainer/master/INFO/changelog.txt";
+
+        private readonly String OtherText =
+            @"©" + DateTime.Now.Year +
+            @" Andrei Gec (http://www.andreigec.net)
+
+Licensed under GNU LGPL (http://www.gnu.org/)
+
+Zip Assets © SharpZipLib (http://www.sharpdevelop.net/OpenSource/SharpZipLib/)
+";
+        #endregion
+
+        public static HistogramOCR h;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var sd = new Licensing.SolutionDetails(HelpString, AppTitle, AppVersion, OtherText, VersionPath, UpdatePath,
+                                                   ChangelogPath);
+            Licensing.CreateLicense(this, sd, menuStrip1);
+
+            h = new HistogramOCR();
+            RefreshLettersList();
+        }
+
+        private void RefreshLettersList()
+        {
+            lettersLB.Items.Clear();
+            letterhistogramdisplayTB.Text = "";
+
+            h.Letters = h.Letters.OrderBy(s => s.Letter).ToList();
+            foreach (var l in h.Letters)
+            {
+                lettersLB.Items.Add(l.Letter);
+            }
+        }
+
+        private void lettersLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            letterhistogramdisplayTB.Text = "";
+            var sel = lettersLB.SelectedIndex;
+            if (sel == -1)
+                return;
+
+            var letter = ((char)lettersLB.Items[sel]);
+
+            SetHistogramDisplay(h.Letters.FirstOrDefault(s => s.Letter.Equals(letter)));
+
+
+        }
+
+        private void SetHistogramDisplay(HistogramOCR.HistogramLetter hl)
+        {
+            if (hl == null)
+                return;
+
+            //merge arrays
+            int[][] a = ArrayExtras.InstantiateArray<int>(h.HistogramWidth, h.HistogramHeight);
+            int yc = hl.YValues.Count();
+            int xc = hl.XValues.Count();
+            for (int y = 0; y < yc; y++)
+            {
+                for (int v = 0; v < hl.YValues[y]; v++)
+                {
+                    if (y >= h.HistogramHeight || v >= h.HistogramWidth)
+                        continue;
+                    a[y][v] += 10;
+                }
+            }
+
+            for (int x = 0; x < xc; x++)
+            {
+                var ymax = yc - 1;
+                var ymin = ymax - hl.XValues[x];
+                for (int v = ymax; v > ymin; v--)
+                {
+                    if (v >= h.HistogramHeight || x >= h.HistogramWidth)
+                        continue;
+                    a[v][x] += 100;
+                }
+            }
+
+            //rotate array
+            // a=ArrayExtras.RotateArray(a, h.HistogramWidth, h.HistogramHeight);
+            //a = ArrayExtras.RotateArray(a, h.HistogramWidth, h.HistogramHeight);
+
+            //print array
+            for (int y = 0; y < yc; y++)
+            {
+                for (int x = 0; x < xc; x++)
+                {
+                    if (y >= h.HistogramHeight || x >= h.HistogramWidth)
+                        continue;
+
+                    var v1 = a[y][x];
+                    var c = '1';
+                    if (v1 == 10)
+                        c = '4';
+                    else if (v1 == 100)
+                        c = '6';
+                    else if (v1 == 110)
+                        c = '8';
+
+                    letterhistogramdisplayTB.Text += c;
+                }
+                letterhistogramdisplayTB.Text += Environment.NewLine;
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Title = "select histogram file to load";
+            ofd.Filter = SaveFileDialogExtras.createFilter("histogram file", "*.hist");
+            ofd.Multiselect = false;
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            var res = ofd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            var fn = ofd.FileName;
+
+            h = HistogramOCR.DeSerialise(fn);
+
+            RefreshLettersList();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveHist();
+        }
+
+        private void SaveHist()
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Title = "enter name/location to save histogram file";
+            sfd.Filter = SaveFileDialogExtras.createFilter("histogram file", "*.hist");
+            sfd.InitialDirectory = Directory.GetCurrentDirectory();
+            var res = sfd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            var fn = sfd.FileName;
+            h.Serialise(fn);
+        }
+
+        private void TestLoadBitmap(Bitmap b)
+        {
+            //split into characters
+            var images = h.SplitUp(b);
+
+            //load white space straight away
+            if (h.Letters.Any(s=>s.Letter.Equals(' '))==false)
+            h.Train(HistogramOCR.WhiteBitmap, ' ');
+
+            //for each image that hasnt been seen, have the player enter it
+            foreach (var row in images)
+            {
+                foreach (var col in row)
+                {
+                    //try and find a perfect match
+                    var c = h.PerformOCRCharacterPerfect(col);
+                    //we only want things we havent seen before
+                    if (c != null)
+                        continue;
+
+                    if (BitmapExtras.IsOnlyColour(col, Color.White))
+                    {
+                        continue;
+                    }
+
+                    //ask user for character and show
+                    var gsi = new GetStringImageCompare();
+                    var letters = gsi.ShowDialog("What character is this? nothing to skip", "question", col,
+                                                 1);
+
+                    if (letters == null)
+                        return;
+
+                    if (letters.Length==0)
+                    {
+                        continue;
+
+                    }
+                    var letter = letters.First().First();
+                    if (char.IsWhiteSpace(letter))
+                    {
+                        MessageBox.Show("cannot be white space");
+                        return;
+                    }
+
+                    if(h.Letters.Any(s=>s.Letter.Equals(letter)))
+                    {
+                        h.Letters.RemoveAll(s=>s.Letter.Equals(letter));
+                    }
+                    
+                    //train
+                    h.Train(col, letter);
+                }
+            }
+        }
+
+        private void addletters_Click(object sender, EventArgs e)
+        {
+            //get image
+            var ofd = new OpenFileDialog();
+            ofd.Title = "select image file that contains separate black letters on a white background";
+            ofd.Multiselect = false;
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            var res = ofd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            var fn = ofd.FileName;
+            Bitmap b;
+            try
+            {
+                b = new Bitmap(fn);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error loading image:" + ex.ToString());
+                return;
+            }
+
+            TestLoadBitmap(b);
+            RefreshLettersList();
+
+            res = MessageBox.Show("do you want to save?", "question", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                SaveHist();
+            }
+        }
+
+        private void loadButtonB_Click(object sender, EventArgs e)
+        {
+            //get image
+            var ofd = new OpenFileDialog();
+            ofd.Title = "select image file that contains separate black letters on a white background";
+            ofd.Multiselect = false;
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            var res = ofd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            var fn = ofd.FileName;
+
+            try
+            {
+                testTextBox.Text = "";
+                var wf = h.PerformOCR(fn, 50);
+                foreach (var row in wf)
+                {
+                    testTextBox.Text += row + "\r\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. make sure you have the .hist file loaded");
+            }
+
+        }
+
+    }
+}
